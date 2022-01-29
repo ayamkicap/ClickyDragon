4df4bda60b2f5dce6748b9420bd6ad608eaa618d
ayamkicap <asrihafiz00@gmail.com>
2022-01-30 01:05:26 +0800

    first commit

---
+++ src/module/view/Game.java
@@ -0,0 +1,151 @@
+package module.view;
+
+import module.controller.Controller;
+import module.model.Dragon;
+import module.model.Tube;
+import module.model.TubeColumn;
+import module.model.soundEffect;
+import module.model.proxy.ProxyImage;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Game extends JPanel implements ActionListener {
+
+    private boolean isRunning = false;
+    private ProxyImage proxyImage;
+    private Image background;
+    private Dragon dragon;
+    private TubeColumn tubeColumn;
+    private int score;
+    private int highScore;
+    public static boolean MUTE = false;
+
+    public Game() {
+
+        proxyImage = new ProxyImage("/resource/Background3.jpeg");
+        background = proxyImage.loadImage().getImage();
+        setFocusable(true);
+        setDoubleBuffered(false);
+        addKeyListener(new GameKeyAdapter());
+        Timer timer = new Timer(15, this);
+        timer.start();
+        new soundEffect(MUTE);
+
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Toolkit.getDefaultToolkit().sync();
+        if (isRunning) {
+            ////////////////////////////////
+            dragon.tick();
+            tubeColumn.tick();
+            checkColision();
+            setScore(getScore() + 1);
+            ///////////////////////////////
+        }
+
+        repaint();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.drawImage(background, 0, 0, null);
+        if (isRunning) {
+            ///////////////////////////////
+            this.dragon.render(g2, this);
+            this.tubeColumn.render(g2, this);
+            g2.setColor(Color.black);
+            g.setFont(new Font("Arial", 1, 20));
+            g2.drawString("Your score: " + this.tubeColumn.getPoints(), 10, 20);
+            ///////////////////////////////
+        } else {
+            g2.setColor(Color.black);
+            g.setFont(new Font("Monospaced", Font.BOLD, 20));
+            g2.drawString("Press 'Enter' to Start", Main.WIDTH / 2 - 130, Main.HEIGHT / 2 - 150);
+            // g2.setColor(Color.black);
+            // g.setFont(new Font("Arial", 50, 15));
+        }
+        g2.setColor(Color.black);
+        g.setFont(new Font("Arial", 1, 20));
+        g2.drawString("High Score: " + highScore, Main.WIDTH - 160, 20);
+
+        g.dispose();
+    }
+
+    private void restartGame() {
+        if (!isRunning) {
+            this.isRunning = true;
+            this.dragon = new Dragon(Main.WIDTH / 2, Main.HEIGHT / 2);
+            this.tubeColumn = new TubeColumn();
+            // new soundEffect(MUTE);
+        }
+    }
+
+    private void endGame() {
+        this.isRunning = false;
+        if (this.tubeColumn.getPoints() > highScore) {
+            this.highScore = this.tubeColumn.getPoints();
+        }
+        this.tubeColumn.setPoints(0);
+        // new soundEffect(!MUTE);
+
+    }
+
+    private void checkColision() {
+        Rectangle rectDragon = this.dragon.getBounds();
+        Rectangle rectTube;
+
+        for (int i = 0; i < this.tubeColumn.getTubes().size(); i++) {
+            Tube tempTube = this.tubeColumn.getTubes().get(i);
+            rectTube = tempTube.getBounds();
+            if (rectDragon.intersects(rectTube)) {
+                endGame();
+            }
+        }
+    }
+
+    // Key
+    private class GameKeyAdapter extends KeyAdapter {
+
+        private final Controller controller;
+
+        public GameKeyAdapter() {
+            controller = new Controller();
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                restartGame();
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            if (isRunning) {
+                controller.controllerReleased(dragon, e);
+            }
+        }
+    }
+}
---
+++ src/module/view/Main.java
@@ -0,0 +1,42 @@
+package module.view;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Main {
+    public static int WIDTH = 800;
+    public static int HEIGHT = 600;
+
+    public Main(int width, int height, String title, Game game) {
+        JFrame frame = new JFrame();
+        frame.add(game);
+        frame.setTitle(title);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMaximumSize(new Dimension(width, height));
+        frame.setPreferredSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+    }
+
+    public static void main (String[] args) {
+        Game game = new Game();
+
+        try {
+            javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        // </editor-fold>
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+
+            Main main= new Main(WIDTH, HEIGHT, "Clicky Dragon", game);
+
+        });
+    }
+}
+
