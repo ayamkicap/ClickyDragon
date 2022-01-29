cc892091ad39b93c354e6aefd07f92e08c14285e
azrulazree <azrulazree7023@gmail.com>
2022-01-30 02:38:11 +0800

    Add comment in Dragon.java

--- src/module/model/Dragon.java
+++ src/module/model/Dragon.java
@@ -6,12 +6,13 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.image.ImageObserver;
 
+//For input and control of the dragon's movement
 public class Dragon extends GameObject {
 
     private ProxyImage proxyImage;
     private Tube[] tube;
 
-    public Dragon(int x, int y) {
+    public Dragon(int x, int y) {  //Control the movement of the dragon
         super(x, y);
         if (proxyImage == null) {
             proxyImage = new ProxyImage("/resource/Dragon5.gif");
@@ -27,7 +28,7 @@ public class Dragon extends GameObject {
     }
 
     @Override
-    public void tick() {
+    public void tick() { //For the dragon's hit box
         if (dy < 5) {
             dy += 2;
         }
